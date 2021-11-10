c475b8bc50a794eea95ee6b4cb1017191cd417a2
Shigeru GOUGI <sgougi@gmail.com>
2021-11-10 22:27:29 +0900

    元に戻す。

--- start_container.bat
+++ start_container.bat
@@ -1,3 +1,3 @@
-Rem docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda --mount type=bind,src=%CD%\workdir,dst=/home/jupyter/workdir anaconda
-docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda -v %CD%\workdir,/home/jupyter/workdir anaconda
+docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda --mount type=bind,src=%CD%\workdir,dst=/home/jupyter/workdir anaconda
+Rem docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda -v %CD%\workdir,/home/jupyter/workdir anaconda
 
