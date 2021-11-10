fb83634271bc6224ea65d821aea374a84b492aea
Shigeru GOUGI <sgougi@gmail.com>
2021-11-10 21:16:25 +0900

    Windows上でのマウント方法を変更

--- start_container.bat
+++ start_container.bat
@@ -1,2 +1,3 @@
-docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda --mount type=bind,src=%CD%\workdir,dst=/home/jupyter/workdir anaconda
+Rem docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda --mount type=bind,src=%CD%\workdir,dst=/home/jupyter/workdir anaconda
+docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda -v %CD%\workdir,/home/jupyter/workdir anaconda
 
