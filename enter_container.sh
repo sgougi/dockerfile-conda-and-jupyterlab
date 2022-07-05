e7daccf586e5c9e10769aaca6140397c5e003af2
ごうぎ しげる <gougi@wingnest.com>
2022-07-06 08:40:10 +0900

    少し修正

--- enter_container.sh
+++ enter_container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+cd $(dirname $0)
 exec docker exec -i -t anaconda su - jupyter
--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
+cd $(dirname $0)
 exec docker run -it -p 222:22 -p 8888:8888 --rm --name anaconda --mount type=bind,src=`pwd`/workdir,dst=/home/jupyter/workdir anaconda
 
