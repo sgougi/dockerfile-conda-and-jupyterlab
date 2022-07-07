7f0988000f312597275987683c81adcd54a46f0a
ごうぎ しげる <gougi@wingnest.com>
2022-07-07 22:07:23 +0900

    conda　init bashを追加。

--- script/setup_anaconda_env.sh
+++ script/setup_anaconda_env.sh
@@ -4,18 +4,15 @@ PYTHON_VERSION=3.8
 
 JUPYTER_HOME=/home/jupyter
 (
-
 	cd ${JUPYTER_HOME}
 
-	mv .bashrc .bashrc.back
-
+	rm .bashrc
+	
 	curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 	./bin/micromamba shell init -s bash -p ${JUPYTER_HOME}
-
-	cat .bashrc .bashrc.back > .bashrc.new
-	mv .bashrc.new .bashrc
 
 	source .bashrc
 	micromamba activate
 	micromamba install mamba python=${PYTHON_VERSION} jupyterlab -c conda-forge
+	conda init bash
 )
